bc1c3b68ecb91b02aac5b976b588b7f8c7b38f1e
keyosh0121 <k.guitar121@gmail.com>
2018-01-13 20:11:57 +0900

    issue

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,8 +1,8 @@
 class ReservationMailer < ApplicationMailer
   def reservation_verify_mail(reservation)
     @reservation = reservation
-    west_or_east = "関西公演" if @reservation.side == "west"
-    west_or_east = "関東公演" if @reservation.side == "east"
+    west_or_east = "関西公演" if @reservation.side == 1
+    west_or_east = "関東公演" if @reservation.side == 0
     mail(
       subject: "Tasky5 × Evans' blue #{west_or_east}ご予約ありがとうございます",
       to: @reservation.email,) do |format|
